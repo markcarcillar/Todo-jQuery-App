0bca16cf7e05f36ea44d35038f96587fb2b18a91
Mark Windows 10 Ubuntu <markcarcillar04@gmail.com>
2020-11-12 22:36:57 +0800

    Fix adding todos
    - Check if do is already in todos
    - Check if its not empty
    - Raise alert error when do is already in todos

--- script.js
+++ script.js
@@ -1,16 +1,22 @@
-$(function(){
-  $('#addToDoBtn').click(function(){
-    let todos = [];
-    let todo = $('#do').val();
-    if (() && (todo in todos)) {
-      $('#todos').prepend(`<div id="todo-${todo}"  class="col-lg-6"><div class="card card-body m-2">${todo}</div></div>`);
-      todos.append(todo);
-      $('#todoError').remove();
-    } else if () {
+// This jQuery have some explanation. I hope you like it!
 
-    } else {
+$(function(){
+  // Place for storing todos, it will be use for checking
+  let todos = [];
+  // Trigger click event then do the code below
+  $('#addToDoBtn').click(function(){
+    // Get the input value
+    let todo = $('#do').val();
+    // Check input if that is already in todos
+    if (todos.includes(todo)) {
+      $('#todoError').remove();
+      $('#todoForm').prepend(`<p id="todoError" class="alert alert-danger text-center">${todo} is already in todos.</p>`)
+    } else if (todo != '') { // Check input if its not empty.
+      $('#todos').prepend(`<div id="todo-${todo}"  class="col-lg-6"><div class="card card-body m-2">${todo}</div></div>`);
+      todos.unshift(todo);
+      $('#todoError').remove();
+    } else { // Gives error when those conditions are not met
       $('#todoForm').prepend(`<p id="todoError" class="alert alert-danger text-center">Todo input should not be empty.</p>`)
     }
-    
   });
 });
