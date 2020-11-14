57ed0f7ac0ddf8ba1acfd24a586ac0fcedee6d9a
Mark Windows 10 Ubuntu <markcarcillar04@gmail.com>
2020-11-14 21:58:10 +0800

    1. Add functionality for clearing all todos 2. Add README, LICENSE, and images for README.

---
+++ README.md
@@ -0,0 +1,28 @@
+# Todo App
+
+- This application is made with HTML, CSS, and jQuery.
+- I made this app just for practicing my skill in jQuery.
+
+Here are the things that you can do here:
+1. Add todo - Add that through input. You can also submit a todo by hitting enter in keyboard. There are rules for creating todos, first is it should not be empty and last is it should not be repeated.
+2. Clear all todos - You can do this by hitting the Del (Delete) on keyboard pr click the Clear All Todos button.
+
+Here are the things that I need to add here:
+1. Select and remove a todo individually.
+2. Add dates on each todo, when it was created.
+
+Here are some examples of this project:
+1. Default/When there are no todos:
+  ![](img/no-todos.png)
+2. With some todos:
+  ![](img/with-some-todos.png)
+3. Adding todo that already exist:
+  ![](img/adding-todo-that-already-exist.png)
+4. Adding todo that is empty:
+  ![](img/adding-todo-that-is-empty.png)
+
+Here what you need to do in order to get/clone this, but first make sure you have git in your terminal/CMD.
+1. On terminal/CMD do the following command:
+  - git clone https://github.com/clediscover/Todo-App.git
+2. Go to the directory, where you've cloned, then open the "index.html".
+3. It's done!
--- script.js
+++ script.js
@@ -1,32 +1,91 @@
-// This jQuery have some explanation. I hope you like it!
+// This jQuery have some explanation using comment. I hope you like it!
+// By: Mark Angelo Carcillar
 
 $(function(){
   // Place for storing todos, it will be use for checking if object is already existed
   let todos = [];
-  
+
   // Trigger click event then do the code below
   $('#addToDoBtn').click(function(){
     // Get the input value
     let todo = $('#do').val();
 
+    let selectedTodos = [];
+  
     // Check input if that is already in todos
     if (todos.includes(todo)) {
-      $('#todoError').remove();
-      $('#todoForm').prepend(`<p id="todoError" class="alert alert-danger text-center">${todo} is already in todos.</p>`)
+      $('.todoError').remove();
+      $('#todoForm').prepend(`<p class="todoError alert alert-danger text-center">${todo} is already in todos.</p>`)
     } else if (todo != '') { // Check input if its not empty.
-      $('#todoError').remove();
-      $('#todos').prepend(`<div class="col-lg-6 pointer"><div id="${todo}-todo" class="card card-body m-2 todo">${todo}</div></div>`);
+      $('.todoError').remove();
+      $('#todos').prepend(`<div class="col-lg-6 pointer"><div class="card card-body m-2 todo">${todo}</div></div>`);
       todos.unshift(todo);
+
+      $('.todo').on('click', function(){
+        let todo = $(this).text()
+        if (selectedTodos.includes(todo)){
+          let indexOfTodo = selectedTodos.indexOf(todo);
+          $(this).removeClass('todo-border');
+          selectedTodos.splice(indexOfTodo, 1);
+          
+        } else if (!selectedTodos.includes(todo)) {
+          $(this).addClass('todo-border');
+          selectedTodos.unshift(todo);
+        }
+
+        // NOT DONE YET - For removing the selected todos.
+        // if (selectedTodos.length > 0){
+        //   $('#todosContainer').append('<div class="row my-3"><div class="col-md-8 col-lg-7 mx-auto"><button id="clearAllSelectedTodos" class="btn btn-block btn-outline-danger">Clear All Selected Todos</button></div></div>');
+        // } else if (selectedTodos.length <= 0) {
+        //   $('#clearAllSelectedTodos').remove();
+        // }
+
+        // $('#clearAllSelectedTodos').click(function(){
+
+        // });
+
+      });
     } else { // Gives error when those conditions are not met
-      $('#todoForm').prepend(`<p id="todoError" class="alert alert-danger text-center">Todo input should not be empty.</p>`)
+      $('.todoError').remove();
+      $('#todoForm').prepend(`<p class="todoError alert alert-danger text-center">Todo input should not be empty.</p>`)
     }
     
     // Remove the value of input
     $('#do').val('');
-
+  
     // Remove div#noTodos when there is todo
     if (todos.length != 0) {
-      $('#noTodos').remove();
+      $('#noTodos').hide();
+      $('#clearTodos').show();
+    }
+  });
+  
+  if (todos.length <= 0) {
+    $('#clearTodos').hide();
+  }
+
+  // Submit input when user hits enter
+  $(document).keydown(function(event){
+    if (event.which == 13){
+      $('#addToDoBtn').trigger('click');
+      event.preventDefault();
+    }
+  });
+
+  // Clear todos
+  $('#clearTodos').click(function(){
+    $('#todos').empty();
+    $('#clearTodos').hide();
+    $('#noTodos').show();
+    $('.todoError').remove();
+    selectedTodos = [];
+    todos = [];
+  });
+
+  // Trigger #clearTodos when Del on keyboard has been clicked
+  $(document).keydown(function(event){
+    if (event.which == 46) {
+      $('#clearTodos').trigger('click');
     }
   });
 });
